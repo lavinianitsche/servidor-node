60d12e013c6c00bece8c0b877280956d8a06361d
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-09-08 12:23:23 -0300

    update mysql

--- server.js
+++ server.js
@@ -16,9 +16,22 @@ connection.connect((err) => {
         console.error('erro ao conectar ao mysql: ', err.stack);
         return;
     }
-    console.log('conectado ao mysql com sucesso!');
-});
 
+    console.log('conectado ao mysql com sucesso!');
+
+    // cria a tabela 'alunos' caso ela não exista
+    const createTableQuery = `CREATE TABLE IF NOT EXISTS alunos(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL
+    )`;
+
+    connection.query(createTableQuery, (err) => {
+        if (err) {
+            console.error('erro ao criar tabela: ', err.stack);
+            return;
+        }
+    });
+});
 
 // define o endereço (localhost) e a porta onde o servidor vai escutar
 const hostname = '127.0.0.1';
@@ -32,7 +45,18 @@ const server = http.createServer((req, res) => {
         return res.end('<h1>página inicial</h1>'); // o return impede a execução das linhas de baixo
     }
 
-    if (req.url === '/alunos') {
+    if (req.url === '/alunos' && req.method === 'GET') {
+        connection.query('SELECT * FROM alunos;', (err, results) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/html; charset=utf-8'});
+                res.end(JSON.stringify({erro: err.message}));
+                return;
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(JSON.stringify(results));
+        });
+
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         return res.end('<h1>lista de alunos</h1>'); // o return impede a execução das linhas de baixo
     }
@@ -42,8 +66,6 @@ const server = http.createServer((req, res) => {
 
     // envia a resposta para o navegador/cliente
     res.end('<h1 style="color: red;">404 - not found in the system</h1>');
-
-
 });
 
 // faz o servidor começar a escutar na porta definida
