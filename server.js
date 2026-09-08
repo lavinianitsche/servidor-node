e3fc8396471333fe8c34100da8ed14f55f4e9756
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-09-08 11:41:43 -0300

    update mysql

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Importa o módulo nativo 'http' do Node.js
+// importa o módulo nativo 'http' do node.js
 const http = require('http');
 const mysql = require('mysql2');
 
@@ -20,13 +20,11 @@ connection.connect((err) => {
 });
 
 
-
-
-// Define o endereço (localhost) e a porta onde o servidor vai escutar
+// define o endereço (localhost) e a porta onde o servidor vai escutar
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// Cria o servidor web
+// cria o servidor web
 const server = http.createServer((req, res) => {
 
     if (req.url === '/') {
@@ -40,15 +38,15 @@ const server = http.createServer((req, res) => {
     }
 
     // se nenhuma rota acima for satisfeita, cai no 404
-    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
 
-    // Envia a resposta para o navegador/cliente
+    // envia a resposta para o navegador/cliente
     res.end('<h1 style="color: red;">404 - not found in the system</h1>');
 
 
 });
 
-// Faz o servidor começar a escutar na porta definida
+// faz o servidor começar a escutar na porta definida
 server.listen(port, hostname, () => {
     console.log(`Servidor rodando em http://${hostname}:${port}/`);
 });
