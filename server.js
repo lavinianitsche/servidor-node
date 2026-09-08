804eb06e6013d03bf05d168b129845d321daadb8
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-09-08 08:14:23 -0300

    update

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = 3000;
 // Cria o servidor web
 const server = http.createServer((req, res) => {
   // Define o status HTTP como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
  
   // Envia a resposta para o navegador/cliente
-  res.end('Olá, alunos! O servidor Node.js está rodando com sucesso!\n');
+  res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n</h1>');
 });
 
 // Faz o servidor começar a escutar na porta definida
