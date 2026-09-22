237d45888f593fa7b845297ad7b7e70b58472535
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-09-22 12:14:35 -0300

    FUNCIONOU PORRAAAA

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
+
+// usar o cors
+app.use(cors())
 
 // middleware para o express entender json no corpo da requisição (req.body)
 app.use(express.json());
